7edaf9b3d8336670ab6162db07e3df1ab71eb31b
Khanim-1 <151565529+Khanim-1@users.noreply.github.com>
2023-11-28 19:55:39 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let count = 0
+
+console.log(saveEl)
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+function save() {
+    let main = count  + " - "
+saveEl.textContent += main
+    console.log(count)
+countEl.textContent = 0
+count= 0
+}
+
